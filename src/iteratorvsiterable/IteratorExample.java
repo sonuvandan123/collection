2f9c5a8c0aab542145949ba19831c8e523e88ac0
sonuvandan123 <vandanmishra6869@gmail.com>
2020-06-05 11:34:03 +0530

    from laptop second commit

--- src/iteratorvsiterable/IteratorExample.java
+++ src/iteratorvsiterable/IteratorExample.java
@@ -5,6 +5,8 @@ public class IteratorExample {
 	public static void main(String[] args) {
 		System.out.println("Git hub code attached from infogain computer");
 		System.out.println("Or you can from second person who clone the code");
+		System.out.println("line added from laptop");
+
 	}
 
 }
